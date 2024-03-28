7a71ed47dbf6e9ef2afec6283bfcd1b97570c266
BritishGamer-40 <karamassaf3@gmail.com>
2024-03-28 12:59:15 -0400

    Navigation bar and fragments done

--- app/src/main/java/com/cs388/socialsync/MainActivity.kt
+++ app/src/main/java/com/cs388/socialsync/MainActivity.kt
@@ -2,6 +2,9 @@ package com.cs388.socialsync
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.cs388.socialsync.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.Firebase
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.IgnoreExtraProperties
@@ -10,9 +13,17 @@ import com.google.firebase.database.database
 class MainActivity : AppCompatActivity() {
 
     private lateinit var database: DatabaseReference
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var dashboardFragment: Fragment
+    private lateinit var eventsFragment: Fragment
+    private lateinit var profileFragment: Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         database = Firebase.database.reference
 
 
@@ -22,11 +33,30 @@ class MainActivity : AppCompatActivity() {
             database.child("users").child(userId).setValue(user)
         }
 
-        writeNewUser("1111", "Cram", "cram@cramer.com")
+        //writeNewUser("1112", "Cram", "cram@cramer.com")
 
+        eventsFragment = EventsFragment() // Initialize homeFragment
+        dashboardFragment = DashboardFragment()
+        profileFragment = ProfileFragment()
 
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val selectedFragment: Fragment = when (item.itemId) {
+                R.id.nav_dashboard -> dashboardFragment
+                R.id.nav_events -> eventsFragment
+                R.id.nav_profile -> profileFragment
 
+                else -> throw IllegalArgumentException("Invalid item ID")
+            }
+            replaceFragment(selectedFragment)
+            true
+        }
 
+        // Set default selection
+        if (savedInstanceState == null) {
+            bottomNavigationView.selectedItemId = R.id.nav_dashboard
+        }
 
     }
 
@@ -34,5 +64,11 @@ class MainActivity : AppCompatActivity() {
     data class User(val username: String? = null, val email: String? = null) {
         // Null default values create a no-argument default constructor, which is needed
         // for deserialization from a DataSnapshot.
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
