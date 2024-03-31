b8bd08500e4066d6c905e4b832c0282d9840ddb0
Saketh Puramsetti <saketh.puramsetti@outlook.com>
2024-03-31 15:30:28 -0400

    Added google auth and styling for login page

--- app/src/main/java/com/cs388/socialsync/activity/LoginActivity.kt
+++ app/src/main/java/com/cs388/socialsync/activity/LoginActivity.kt
@@ -12,13 +12,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.android.material.button.MaterialButton
+
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var ivGoogleLogin: ImageView
+    lateinit var ivGoogleLogin: MaterialButton
 
     lateinit var auth: FirebaseAuth
 
