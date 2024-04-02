26164524fe19335af16bad32d00a8ac2754d0861
Mikel Alcazar <mklalcazar@gmail.com>
2024-04-02 13:22:27 -0400

    Started Change Time Screen

---
+++ app/src/main/java/com/cs388/socialsync/ChangeTimeActivity.kt
@@ -0,0 +1,12 @@
+package com.cs388.socialsync
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class ChangeTimeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_time)
+
+        // Any initialization or setup code can go here
+    }
+}
--- app/src/main/java/com/cs388/socialsync/EventsFragment.kt
+++ app/src/main/java/com/cs388/socialsync/EventsFragment.kt
@@ -1,14 +1,24 @@
 package com.cs388.socialsync
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 
 class EventsFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var changeTimeButton : Button
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        changeTimeButton = view.findViewById(R.id.changeTimeButton)
+        changeTimeButton.setOnClickListener {
+            val intent = Intent(requireContext(), ChangeTimeActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onCreateView(
