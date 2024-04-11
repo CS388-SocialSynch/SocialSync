a74c4ab7baca8bc247e4f66411c1a0b09cf41e74
Mikel Alcazar <mklalcazar@gmail.com>
2024-04-11 14:59:35 -0400

    Expanded shadow of buttons

--- app/src/main/java/com/cs388/socialsync/DateAdapter.kt
+++ app/src/main/java/com/cs388/socialsync/DateAdapter.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import java.time.LocalDate
@@ -41,9 +44,12 @@ class DateAdapter(
         private val dateTV: TextView = itemView.findViewById(R.id.date_item_date)
         private val dayTV: TextView = itemView.findViewById(R.id.date_item_dayAbv)
         private val underline: View = itemView.findViewById(R.id.date_item_underline)
+        private val dateButton: LinearLayout = itemView.findViewById(R.id.dateButton)
 
         init {
             itemView.setOnClickListener(this)
+
+
         }
         fun bind(date: String, isSelected:Boolean) {
             val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
@@ -51,7 +57,11 @@ class DateAdapter(
             val currDate = LocalDate.parse(date, inputFormatter)
             dateTV.text =  currDate.format(outputFormatter)
             dayTV.text = currDate.dayOfWeek.toString().substring(0, 3)
-            dateTV.paint.isUnderlineText = isSelected
+            if (isSelected){
+                dateButton.background=AppCompatResources.getDrawable(itemView.context,R.drawable.selected_date)
+            }else{
+                dateButton.background=AppCompatResources.getDrawable(itemView.context,R.drawable.unselected_date)
+            }
             underline.visibility =  if (isSelected) View.VISIBLE else View.GONE
         }
 
