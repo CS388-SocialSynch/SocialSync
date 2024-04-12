b0ff6c62c4066db6e0b6578317600f77cf9a772f
Saketh Puramsetti <saketh.puramsetti@outlook.com>
2024-04-11 22:44:51 -0400

    Got data from predict HQ API

--- app/src/main/java/com/cs388/socialsync/EventAdapter.kt
+++ app/src/main/java/com/cs388/socialsync/EventAdapter.kt
@@ -29,7 +29,7 @@ class EventAdapter(private val context: Context, private val eventList: List<Eve
         val dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
 
         holder.eventNameTextView.text = currentItem.eventName
-        "${currentItem.startTime.format(timeFormatter)} - ${currentItem.endTime.format(timeFormatter)}".also { holder.timeTextView.text = it }
+        "${currentItem.startTime!!.format(timeFormatter)} - ${currentItem.endTime!!.format(timeFormatter)}".also { holder.timeTextView.text = it }
         holder.dateTextView.text = currentItem.date.format(dateFormatter)
         "${currentItem.temperature}°F".also { holder.temperatureTextView.text = it }
 
@@ -75,4 +75,3 @@ class EventAdapter(private val context: Context, private val eventList: List<Eve
 
     }
 }
-
--- app/src/main/java/com/cs388/socialsync/EventsFragment.kt
+++ app/src/main/java/com/cs388/socialsync/EventsFragment.kt
@@ -1,18 +1,28 @@
 package com.cs388.socialsync
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.cs388.socialsync.databinding.FragmentEventsBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
 import java.time.LocalTime
-import kotlin.random.Random
+import java.time.format.DateTimeFormatter
 
 class EventsFragment : Fragment() {
 
@@ -30,58 +40,109 @@ class EventsFragment : Fragment() {
 
         binding.publicEventsRecyclerView.layoutManager = LinearLayoutManager(activity)
 
-        val events: List<Event> = createRandomEvents()
+        val events: ArrayList<Event> = ArrayList()
 
         publicEventAdapter = EventAdapter(requireContext(), events)
         binding.publicEventsRecyclerView.adapter = publicEventAdapter
+        fetchEvents(events)
 
         return view
     }
 
-    private fun createRandomEvents(): List<Event> {
-        val events = mutableListOf<Event>()
-        val eventNames = mutableListOf<String>()
+    fun fetchEvents(eventsList: ArrayList<Event>) {
 
-        // Sample event names
-        val sampleNames = listOf(
-            "Birthday Bash",
-            "Concert Night",
-            "Tech Meetup",
-            "Food Festival",
-            "Art Exhibition",
-            "Networking"
-        )
+        val client = okhttp3.OkHttpClient()
+        val request = okhttp3.Request.Builder()
+            .url("https://api.predicthq.com/v1/events?country=US&location_around.origin=40.7357,-74.1724&location_around.offset=10km&location_around.scale=10km&limit=10")
+            .addHeader("Authorization", "Bearer 4nt22lSWoSHgp9vJz9TRU3zhWjTkdfofuR3Luwol")
+            .addHeader("Accept", "application/json")
+            .build()
 
-        repeat(6) {
-            var eventName = ""
-            do {
-                eventName = sampleNames.random()
-            } while (eventName in eventNames)
+        val queue = Volley.newRequestQueue(context)
+        val url =
+            "https://api.predicthq.com/v1/events?country=US&location_around.origin=40.7357,-74.1724&location_around.offset=10km&location_around.scale=10km&limit=10"
+        val getRequest: StringRequest = object : StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                Log.e("CUSTOM---->", response)
+                val json = JSONObject(response)
+                val resultsArray = json.getJSONArray("results")
+                Log.e("CUSTOM---->", resultsArray.getJSONObject(0).getString("title"))
 
-            eventNames.add(eventName)
 
-            val startTime = LocalTime.of(10,0)//"10 AM"
-            val endTime = LocalTime.of(12,0)//"12 PM"
-            val date = LocalDate.of(2024,4,9)//"04/09/2024"
-            val temperature = Random.nextInt(50, 100)
-            val weatherCondition = if (it % 2 == 0) "cloudy" else "sunny"
-            val location = "NJIT"
-            val address = "CC"
+                for (i in 0 until resultsArray.length()) {
+                    val eventJson = resultsArray.getJSONObject(i)
+                    val title = eventJson.getString("title")
+                    val words = title.split(" ")
+                    val displayTitle = if (words.size >= 4) {
+                        words.take(4).joinToString(" ") + "..."
+                    } else {
+                        title
+                    }
+                    var formattedAddress = "Newark, 07102";
+                    var locationName = "Campus Center"
+                    val entitiesArray = eventJson.getJSONArray("entities")
+                    if (entitiesArray.length() > 0) {
+                        val entity = entitiesArray.getJSONObject(0)
+                        formattedAddress = entity.getString("formatted_address")
+                        locationName = entity.getString("name")
+                        Log.e("Formatted Address", formattedAddress)
+                    }
+                    val startDateTime = eventJson.getString("start")
+                    //val endDateTime = LocalTime.parse(eventJson.getString("end"), DateTimeFormatter.ISO_DATE_TIME)
+                    //val formattedAddress = "unknown"
+                    //val eventDate = LocalDate.parse(eventJson.getString("start"), DateTimeFormatter.ISO_DATE)
 
-            val event = Event(
-                eventName,
-                startTime,
-                endTime,
-                date,
-                temperature,
-                weatherCondition,
-                location,
-                address,
-                false,
-                true
-            )
-            events.add(event)
+
+                    val event = Event(
+                        eventName = displayTitle,
+                        startTime = LocalTime.now(),
+                        endTime = LocalTime.now(),
+                        date = LocalDate.now(),
+                        temperature = 57,
+                        weatherCondition = "sunny",
+                        locationName = locationName,
+                        address = formattedAddress,
+                        isHost = false,
+                        isPublic = true,
+                        showParticipants = false,
+                        isInPerson = false,
+                        hostUID = "",
+                        optionStartTime = null,
+                        optionEndTime = null,
+                        specificDate = false
+                    )
+                    eventsList.add(event)
+                }
+                publicEventAdapter.notifyDataSetChanged()
+
+
+
+
+
+
+            },
+            Response.ErrorListener {
+                Log.e("CUSTOM---->", it.message.toString())
+            }
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["Authorization"] = "Bearer 4nt22lSWoSHgp9vJz9TRU3zhWjTkdfofuR3Luwol"
+                params["Accept"] = "application/json"
+                return params
+            }
         }
-        return events
+        queue.add(getRequest)
+
+
     }
+
+
+
+
+
+
+
 }
